25eeec69d1c84743123da54a02597866c7bc10a2
Simon Willison <swillison@gmail.com>
2021-12-18 20:29:58 -0800

    Don't break pages with no request, closes #6

--- datasette_pretty_traces/__init__.py
+++ datasette_pretty_traces/__init__.py
@@ -3,7 +3,7 @@ from datasette import hookimpl
 
 @hookimpl
 def extra_js_urls(datasette, request):
-    if request.args.get("_trace") and datasette.setting("trace_debug"):
+    if request and request.args.get("_trace") and datasette.setting("trace_debug"):
         return [
             datasette.urls.static_plugins(
                 "datasette_pretty_traces", "datasette-pretty-traces.js"
--- tests/test_pretty_traces.py
+++ tests/test_pretty_traces.py
@@ -1,4 +1,7 @@
+from datasette import hookimpl
 from datasette.app import Datasette
+from datasette.utils.asgi import Response
+from datasette.plugins import pm
 import pytest
 
 
@@ -24,3 +27,24 @@ async def test_script_included_conditions(trace_debug, _trace, expected):
         assert fragment in response.text
     else:
         assert fragment not in response.text
+
+
+@pytest.mark.asyncio
+async def test_does_not_break_pages_with_no_request():
+    async def hello(datasette):
+        return Response.html(await datasette.render_template("show_json.html"))
+
+    class HelloRoutePlugin:
+        __name__ = "HelloRoutePlugin"
+
+        @hookimpl
+        def register_routes(self):
+            return [(r"^/hello$", hello)]
+
+    pm.register(HelloRoutePlugin(), name="HelloRoutePlugin")
+    try:
+        ds = Datasette(memory=True, files=[])
+        response = await ds.client.get("/hello")
+        assert response.status_code == 200
+    finally:
+        pm.unregister(name="HelloRoutePlugin")
